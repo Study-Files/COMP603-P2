3adbcf1c9ee4a386541184cbb3db7c0e834fab13
TheMysterys <the-mystery@outlook.com>
2022-06-01 17:02:25 +1200

    Added some more tests

--- src/project2/Project2Test.java
+++ src/project2/Project2Test.java
@@ -4,9 +4,11 @@ import org.junit.*;
 import project2.Database.DatabaseController;
 import project2.Database.DatabasePets;
 import project2.Database.DatabaseUser;
+import project2.Pets.Pet;
 import project2.Pets.Types.Dog;
 
 import java.sql.SQLException;
+import java.util.HashSet;
 
 public class Project2Test {
 
@@ -39,6 +41,36 @@ public class Project2Test {
     @Test
     public void testDatabaseLogin() {
         Assert.assertTrue(DatabaseUser.login(database.getConnection(), "test", "test"));
+    }
+
+    @Test
+    public void testGetPets() {
+        Assert.assertEquals(DatabasePets.getPets(database.getConnection(), "test").size(), 1);
+    }
+
+    @Test
+    public void testGetPets2() {
+        Assert.assertEquals(DatabasePets.getPets(database.getConnection(), "jack").size(), 1);
+    }
+
+    @Test
+    public void testIncreasePetAge() {
+        HashSet<Pet> pets = DatabasePets.getPets(database.getConnection(), "test");
+        Pet pet = pets.iterator().next();
+        pet.increaseAge();
+        int expected = 2;
+        int actual = pet.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testIncreasePetAgeFail() {
+        HashSet<Pet> pets = DatabasePets.getPets(database.getConnection(), "jack");
+        Pet pet = pets.iterator().next();
+        pet.increaseAge();
+        int expected = 3;
+        int actual = pet.getAge();
+        Assert.assertEquals(expected, actual);
     }
 
 
